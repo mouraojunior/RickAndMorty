420f107b6584a42b8c5bca3a753e74ab07fb8bec
Junior Mourão <junniormourao@gmail.com>
2022-01-11 14:47:55 -0300

    Dependency Injection: Module and Application

---
+++ app/src/main/java/juniormourao/RickAndMortyApplication.kt
@@ -0,0 +1,7 @@
+package juniormourao
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickAndMortyApplication: Application()
---
+++ app/src/main/java/juniormourao/rickandmorty/di/RickAndMortyModule.kt
@@ -0,0 +1,48 @@
+package juniormourao.rickandmorty.di
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import juniormourao.rickandmorty.data.cache.RickAndMortyDatabase
+import juniormourao.rickandmorty.data.remote.RickAndMortyApi
+import juniormourao.rickandmorty.data.remote.RickAndMortyApi.Companion.BASE_URL_API
+import juniormourao.rickandmorty.data.repository.CharacterRepositoryImpl
+import juniormourao.rickandmorty.domain.repository.CharacterRepository
+import juniormourao.rickandmorty.domain.use_case.GetCharacters
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RickAndMortyModule {
+    @Singleton
+    @Provides
+    fun provideGetCharactersUseCase(
+        getCharactersRepository: CharacterRepository
+    ): GetCharacters = GetCharacters(getCharactersRepository)
+
+    @Singleton
+    @Provides
+    fun provideCharacterRepository(
+        api: RickAndMortyApi,
+        db: RickAndMortyDatabase
+    ): CharacterRepository = CharacterRepositoryImpl(api, db.characterDao)
+
+    @Singleton
+    @Provides
+    fun provideRickAndMortyApi(
+    ): RickAndMortyApi =
+        Retrofit.Builder().baseUrl(BASE_URL_API).addConverterFactory(GsonConverterFactory.create())
+            .build().create(RickAndMortyApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideRickAndMortyDatabase(
+        app: Application
+    ): RickAndMortyDatabase =
+        Room.databaseBuilder(app, RickAndMortyDatabase::class.java, "rickAndMortyDb").build()
+}
