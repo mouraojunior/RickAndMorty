7b31ead1128515841557b5011519c52bd12f3329
Junior Mourão <junniormourao@gmail.com>
2022-01-14 21:08:02 -0300

    Loading state impl

--- app/src/main/java/juniormourao/rickandmorty/presentation/character_list/CharacterListFragment.kt
+++ app/src/main/java/juniormourao/rickandmorty/presentation/character_list/CharacterListFragment.kt
@@ -2,6 +2,8 @@ package juniormourao.rickandmorty.presentation.character_list
 
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -46,10 +48,18 @@ class CharacterListFragment : Fragment(R.layout.fragment_character_list) {
                 characterListViewModel.characterListState.collectLatest { state ->
                     when (state) {
                         is CharacterListState.Error -> {
-                            characterListBinding.root.showSnackBar(state.errorMessage)
+                            characterListBinding.apply {
+                                pbLoading.visibility = GONE
+                                root.showSnackBar(state.errorMessage)
+                            }
                         }
-                        is CharacterListState.Loading -> println("LoadingState")
+                        is CharacterListState.Loading -> {
+                            characterListBinding.apply {
+                                pbLoading.visibility = VISIBLE
+                            }
+                        }
                         is CharacterListState.Success -> {
+                            characterListBinding.pbLoading.visibility = GONE
                             characterListAdapter.submitList(state.characterList)
                         }
                     }
